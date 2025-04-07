59e819cbb0fba52d0aa589a712b9419e577b5006
Truman Yen <yen.truman@gmail.com>
2025-04-07 09:42:34 -0600

    Align results properly in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -28,10 +28,18 @@ def otsuThreshold(img):
 
     return result
 
-warped_images = []
-adaptive_results = []
-otsu_results = []
-result_images = []
+class Results:
+    def __init__(self,
+                 image_with_corners,
+                 warped_image,
+                 adaptive_result,
+                 otsu_result,):
+        self.image_with_corners = image_with_corners
+        self.warped_image = warped_image
+        self.adaptive_result = adaptive_result
+        self.otsu_result = otsu_result
+
+results = []
 input_paths = os.listdir("test_data")
 
 for file in input_paths:
@@ -39,12 +47,11 @@ for file in input_paths:
         file_path = os.path.join("test_data", file)
         #print("Testing with: ", file_path)
         start_time = time.time()
-        results, hull, corners = find_paper_corners(
+        im_with_corners, hull, corners = find_paper_corners(
             file_path=file_path,
             bg_removal_thresh=BG_REMOVAL_THRESH,
             minimum_hough_lines=MIN_HOUGH_LINES,
             display_intermediate_results=DISPLAY_INTERMEDIATE_RESULTS)
-        result_images.append(results)
         #print("Took", str(time.time() - start_time), "seconds")
         #print(corners)
 
@@ -52,37 +59,42 @@ for file in input_paths:
             # get warped image
             img = cv2.imread(file_path)
             warped_img = perspectiveTransform(img, corners)
-            warped_images.append(cv2.cvtColor(warped_img, cv2.COLOR_BGR2RGB))
+            warped_img = cv2.cvtColor(warped_img, cv2.COLOR_BGR2RGB)
             # get thresholded images
             img_gray = cv2.cvtColor(warped_img, cv2.COLOR_BGR2GRAY)
             img_adaptive = adaptiveThreshold(img_gray)
             img_otsu = otsuThreshold(img_gray)
-            adaptive_results.append(img_adaptive)
-            otsu_results.append(img_otsu)
+        else: # No result, use blank images
+            warped_img = np.zeros_like(im_with_corners)
+            img_adaptive = np.zeros_like(im_with_corners)
+            img_otsu = np.zeros_like(im_with_corners)
 
-num_images = len(result_images)
+        results.append(Results(image_with_corners=im_with_corners,
+                               warped_image=warped_img,
+                               adaptive_result=img_adaptive,
+                               otsu_result=img_otsu))
+
+num_images = len(results)
 # num_images = 3
 fig, axes = plt.subplots(4, num_images, figsize=(10 * num_images, 30))
 
 for i in range(num_images):
-    axes[0][i].imshow(result_images[i])
+    result_i = results[i]
+    axes[0][i].imshow(result_i.image_with_corners)
     # axes[0][i].set_title("Detected Corners", fontsize=20)
     axes[0][i].axis("off")
 
-    if i < len(warped_images):
-        axes[1][i].imshow(warped_images[i])
-        # axes[1][i].set_title("Warped Perspective", fontsize=20)
-        axes[1][i].axis("off")  
+    axes[1][i].imshow(result_i.warped_image)
+    # axes[1][i].set_title("Warped Perspective", fontsize=20)
+    axes[1][i].axis("off")  
 
-    if i < len(adaptive_results):
-        axes[2][i].imshow(adaptive_results[i], cmap='gray')
-        # axes[2][i].set_title("Adaptive Thresholding", fontsize=22)
-        axes[2][i].axis("off")
+    axes[2][i].imshow(result_i.adaptive_result, cmap='gray')
+    # axes[2][i].set_title("Adaptive Thresholding", fontsize=22)
+    axes[2][i].axis("off")
 
-    if i < len(otsu_results):
-        axes[3][i].imshow(otsu_results[i], cmap='gray')
-        # axes[3][i].set_title("Otsu Thresholding", fontsize=20)
-        axes[3][i].axis("off")
+    axes[3][i].imshow(result_i.otsu_result, cmap='gray')
+    # axes[3][i].set_title("Otsu Thresholding", fontsize=20)
+    axes[3][i].axis("off")
 
 plt.tight_layout()
 plt.show()
